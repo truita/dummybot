f684f1bc637205807a42fe1e64d3582c4a197273
salmonmolon <coolsalmon@outlook.com>
2020-05-18 17:55:52 +0200

    First database tests

---
+++ database.py
@@ -0,0 +1,9 @@
+import mysql.connector as mysql
+import random
+
+connection = mysql.connect(user="dummybot",host="localhost",database="POLE")
+cursor = connection.cursor()
+
+cursor.execute("SELECT * FROM datos")
+result = cursor.fetchall()
+print(result[random.randint(0,len(result))])
