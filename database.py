7163c3b1d6f08451447dd7e8bca59186b9670db5
salmonmolon <coolsalmon@outlook.com>
2020-05-18 23:24:15 +0200

    You can now add facts

--- database.py
+++ database.py
@@ -4,12 +4,12 @@ import random
 connection = mysql.connect(user="dummybot",host="localhost",database="POLE")
 cursor = connection.cursor()
 
-cursor.execute("SELECT * FROM datos")
-result = cursor.fetchall()
-print(result[random.randint(0,len(result))])
-
 def getFact():
     cursor.execute("SELECT * FROM datos")
     result = cursor.fetchall()
     fact = str(result[random.randint(0,len(result))])[2:-3]
     return fact
+
+def addFact(arg):
+    cursor.execute("INSERT INTO datos(dato) VALUES('{0}')".format(arg))
+    return 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from database import getFact
+from database import getFact,addFact
 import sys
 
 bot = commands.Bot(command_prefix='--',help_command=None)
@@ -16,6 +16,13 @@ async def stop_command(ctx):
 @bot.command(name='dato')
 async def dato_command(ctx):
     await ctx.channel.send(getFact())
+@bot.command(name='datoadd')
+async def datoadd_command(ctx,*,arg1):
+    try:    
+        addFact(arg1)
+        await ctx.channel.send('Dato añadido!')
+    except:
+        await ctx.channel.send('Ha ocurrido un error al añadir tu dato')
 
 
 bot.run('NzAyMDk3MzgxMTg5MDkxNDE5.XsK3GA.IdC8JTZ3N5AbqJrAmVM8u70x2BE')
