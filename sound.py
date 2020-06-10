2378e0857413a1c9c0d02c4dd8a7213dcd43eab9
salmonmolon <coolsalmon@outlook.com>
2020-06-10 18:58:48 +0200

    Now track is passed to function

--- sound.py
+++ sound.py
@@ -22,13 +22,10 @@ async def leave_channel(ctx:commands.Context):
     await voice_client.disconnect()
 
 async def pass_track(ctx):
-    global track
     global player
     player.source = discord.FFmpegOpusAudio(queue[track])
 
-def download(url):
-    filename = ""
-    global track
+def download(url, track):
     youtube_dl.YoutubeDL({'format': 'bestaudio/best', 'outtmpl': '/tmp/dummybot/{0}.webm'.format(track)}).download([url])
     filename = "/tmp/dummybot/{0}.webm".format(track)
     global queue
@@ -37,7 +34,7 @@ def download(url):
 async def play(ctx,url):
     global track
     track += 1
-    download(url)
+    download(url, track)
     guild = ctx.guild
     voice_client = guild.voice_client
     if not voice_client.is_playing() or not voice_client.is_paused():
