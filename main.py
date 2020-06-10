45bff7a696ad9977260414311274a470b2e28f9d
salmonmolon <coolsalmon@outlook.com>
2020-06-10 18:54:10 +0200

    Reorganized code + fix (maybe)

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def ping_command(ctx:commands.Context):
 
 @bot.command(name='version')
 async def version_command(ctx):
-    await ctx.channel.send('dummybot(py) v1.90 NO STABLE') #Sends current bot version over discord
+    await ctx.channel.send('dummybot(py) v1.92 NO STABLE') #Sends current bot version over discord
 
 @bot.command(name='stop')
 async def stop_command(ctx):
--- sound.py
+++ sound.py
@@ -4,7 +4,7 @@ import discord
 import os
 
 queue = []
-track = 0
+track = -1
 
 def initialize_voice(bot_argument:commands.Bot):
     global bot
@@ -24,21 +24,22 @@ async def leave_channel(ctx:commands.Context):
 async def pass_track(ctx):
     global track
     global player
-    track += 1
     player.source = discord.FFmpegOpusAudio(queue[track])
 
-async def play(ctx,url):
+def download(url):
     filename = ""
     global track
+    youtube_dl.YoutubeDL({'format': 'bestaudio/best', 'outtmpl': '/tmp/dummybot/{0}.webm'.format(track)}).download([url])
+    filename = "/tmp/dummybot/{0}.webm".format(track)
+    global queue
+    queue.append(filename)
+    
+async def play(ctx,url):
+    global track
+    track += 1
+    download(url)
     guild = ctx.guild
     voice_client = guild.voice_client
-    with youtube_dl.YoutubeDL({'format': 'bestaudio/best', 'outtmpl': '/tmp/dummybot/{0}.webm'.format(track)}) as ydl:
-        ydl.download([url])
-        filename = "/tmp/dummybot/{0}.webm".format(track)
-        global queue
-        queue.append(filename)
-
-    
     if not voice_client.is_playing() or not voice_client.is_paused():
         global player
         player = await voice_client.play(discord.FFmpegOpusAudio(queue[track]))
