c1b34c0c9d10f95bb7c96db76cf39ce9f6ecd5af
salmonmolon <coolsalmon@outlook.com>
2020-06-11 02:17:27 +0200

    Fixed passing track

--- sound.py
+++ sound.py
@@ -29,7 +29,10 @@ async def pass_track(ctx):
     while not queue[track]:
         asyncio.run(asyncio.sleep(0.5))
     track = track
-    ctx.guild.voice_client.source = discord.FFmpegOpusAudio(queue[track])
+    if not ctx.guild.voice_client.is_playing() or not ctx.guild.voice_client.is_paused():
+        await ctx.guild.voice_client.play(discord.FFmpegOpusAudio(queue[track]))
+    else:
+        ctx.guild.voice_client.source = discord.FFmpegOpusAudio(queue[track])
     track += 1
 
 def download(url, track):
