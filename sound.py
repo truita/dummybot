4e5e45a4060191ab8dee0666dcab110587c320b6
salmonmolon <josepmengual@salmorejo.cbu.net>
2020-07-13 16:02:18 +0200

    Debbuging added

--- sound.py
+++ sound.py
@@ -53,6 +53,9 @@ class MusicManager():
     async def next_song(self, ctx):
         self.guild_tracks[ctx.guild.id] += 1
 
+        print(self.guild_tracks[ctx.guild.id])
+        print(self.guild_queues[ctx.guild.id][self.guild_tracks[ctx.guild.id]])
+
         if self.guild_queues[ctx.guild.id] == None:
             return
         
