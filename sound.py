dea9b83f94b9a4e6835cc4a0dc295e155edb20ce
salmonmolon <josepmengual@salmorejo.cbu.net>
2020-07-14 12:15:49 +0200

    Switch from youtube-dl to Youtube api + ytpipe

--- sound.py
+++ sound.py
@@ -1,7 +1,10 @@
-import youtube_dl
+from pytube import YouTube
+import pyyoutube
 from discord.ext import commands
 import discord
 import os, asyncio
+
+api = pyyoutube.Api(api_key=os.getenv("YOUTUBE_API_KEY"))
 
 class MusicManager():
     DOWNLOAD_PATH = "/tmp/dummybot"
@@ -33,11 +36,10 @@ class MusicManager():
             song_file = "{0}/{1}".format(self.DOWNLOAD_PATH, i)
             self.guild_queues[guild.id].append(song_file)
     
-    async def download(self,url, after):
-        with youtube_dl.YoutubeDL({'format': 'bestaudio/opus','default_search': 'ytsearch1','outtmpl': '{0}/%(id)s'.format(self.DOWNLOAD_PATH), 'nooverwrites': True}) as ydl:
-            for i in url:
-                ydl.download([i])
-                await asyncio.sleep(0.5)
+    async def download(self,url,*, after=None):
+        for song_id in url:
+            YouTube(url="v={0}".format(song_id)).streams.filter(audio_codec="opus", only_audio=True).first().download(output_path="/tmp/dummybot",filename=song_id)
+            await asyncio.sleep(0.1)
         after()
     
     def __do_play__(self,ctx):
@@ -46,26 +48,39 @@ class MusicManager():
         current_song = self.guild_queues[ctx.guild.id][self.guild_tracks[ctx.guild.id]]
         voice_client.play(discord.FFmpegOpusAudio(current_song, codec="copy"), after=lambda a: loop.create_task(self.next_song(ctx)))
     
-    async def play(self,ctx:commands.Context, arg):
+    async def play(self,ctx:commands.Context, arg:str):
         loop = asyncio.get_event_loop()
         if ctx.guild.voice_client == None:
             await self.join_channel(ctx)
 
-        with youtube_dl.YoutubeDL({'format': 'bestaudio/opus', 'default_search': 'ytsearch1'}) as ydl:
-            song_info = ydl.extract_info(arg, False)
+        song_list = []
 
-        song_id = []
-        song_urls = []
-        if "entries" in song_info.keys():
-            for i in song_info["entries"]:
-                song_id.append(i["id"])
-                song_urls.append(i["webpage_url"])
+        if arg.find("playlist") + 1:        #We add 1 because find returns -1 if nothing is found
+            playlist_id = arg[arg.find("list=") + 5 : ]
+            pageToken = None
+            while True:
+                playlist = api.get_playlist_items(playlist_id= playlist_id, page_token=pageToken)
+                await asyncio.sleep(0.1)
+
+                for item in playlist.items:
+                    song_list.append(item.contentDetails.videoId)
+
+                pageToken = playlist.nextPageToken
+
+                if pageToken is None:
+                    break
+        elif arg.find("watch?v=") + 1:
+            if arg.find("&") + 1: #Removes additional video queries (such as list)
+                arg = arg[:arg.find("&")]
+            song_list.append(arg[arg.find("watch?v=") + 8 : ])
         else:
-            song_id.append(song_info["id"])
+            song_list.append(api.search(q=arg).items[0].id.videoId)  
 
-        self.__queue__(ctx.guild, song_id)
+        self.__queue__(ctx.guild, song_list)
         if not ctx.guild.voice_client.is_playing():
-            loop.create_task(self.download(song_urls, lambda: self.__do_play__(ctx)))
+            loop.create_task(self.download(song_list, after=lambda: self.__do_play__(ctx)))
+        else:
+            loop.create_task(self.download(song_list))
 
     async def next_song(self, ctx):
         self.guild_tracks[ctx.guild.id] += 1
