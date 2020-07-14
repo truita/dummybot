f1da49520173c86dbaebc222544a5f8831e391fe
salmonmolon <josepmengual@salmorejo.cbu.net>
2020-07-14 17:34:43 +0200

    Now reacts to .loop and .shuffle

--- sound.py
+++ sound.py
@@ -112,9 +112,11 @@ class MusicManager():
 
     async def shuffle(self,ctx):
         random.shuffle(self.guild_queues[ctx.guild.id])
+        ctx.message.add_reaction("🔀")
 
     async def loop(self,ctx):
         self.guild_loop[ctx.guild.id] = True
+        ctx.message.add_reaction("🔄")
     
     async def show_queue(self, ctx):
         result = "```ml\n"
