586fa3883c01c9641eb9d8a0b79bbb337683e04a
Josep Mengual <salmon@archlinux.local>
2020-12-07 02:09:09 +0100

    Facts now are fully functional

--- flags.py
+++ flags.py
@@ -1,26 +1,26 @@
 import discord
 import sqlite3
 import os
+import database_utils
 import asyncio
 from datetime import datetime, timedelta
 from discord.ext import commands, tasks
 from operator import itemgetter
+
 
 class Flags(commands.Cog):
     readyflags = {}
 
     def __init__(self, bot):
         self.bot = bot
-        workingDirectory = os.path.realpath(os.path.dirname(__file__))
-        self.db_dir = os.path.join(workingDirectory, "db")
-        if not os.path.isdir(self.db_dir):
-            os.mkdir(self.db_dir, 664)
+        sql = """CREATE TABLE IF NOT EXISTS FLAGS (
+                    ID INTEGER NOT NULL PRIMARY KEY, 
+                    POINTS INTEGER DEFAULT 0 NOT NULL,
+                    POLE INTEGER DEFAULT 0 NOT NULL,
+                    SUBPOLE INTEGER DEFAULT 0 NOT NULL,
+                    FAIL INTEGER DEFAULT 0 NOT NULL);"""
+        database_utils.execute_on_all_db(sql)
         self.reset_flags.start()
-
-    def _get_conn(self, guild_id):
-        return sqlite3.connect(
-            os.path.join(self.db_dir, f"{guild_id}.sqlite3"), isolation_level=None
-        )
 
     class FlagChecks():
         pole = True
@@ -39,7 +39,8 @@ class Flags(commands.Cog):
             if flagtype == "fail":
                 return (("pole", self.pole, self.pole_maker), ("subpole", self.subpole, self.subpole_maker))
 
-    def is_flag_ready(self, flagtype, guild, maker): # 0 = Flag is unavalaible, 1 = Flag is avalaible, *FlagType* = A user has already taken *FlagType* 
+    # 0 = Flag is unavalaible, 1 = Flag is avalaible, *FlagType* = A user has already taken *FlagType*
+    def is_flag_ready(self, flagtype, guild, maker):
         if guild in self.readyflags:
             if not getattr(self.readyflags[guild], flagtype):
                 return 0
@@ -47,29 +48,27 @@ class Flags(commands.Cog):
                 if pack[2] == maker:
                     return pack[0]
             return 1
-            
+
         else:
             self.readyflags[guild] = self.FlagChecks()
-            setattr(self.readyflags[guild], f"{flagtype}_maker", False)
-            return 1
+            setattr(self.readyflags[guild], f"{flagtype}", False)
+            return 0
 
-    
-    
     @tasks.loop(hours=24)
     async def reset_flags(self):
         for guild in self.readyflags:
-            
+
             guild.pole = True
             guild.subpole = True
             guild.fail = True
-            
+
             guild.pole_maker = 0
             guild.subpole_maker = 0
             guild.fail_maker = 0
-    
+
     @reset_flags.before_loop
     async def before_reset_flags(self):
-        hour = 00 
+        hour = 00
         minute = 00
         now = datetime.now()
         target = datetime(now.year, now.month, now.day, hour, minute)
@@ -79,17 +78,15 @@ class Flags(commands.Cog):
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
-        conn = self._get_conn(guild.id)
-        sql = (
-            """CREATE TABLE FLAGS (
-                        ID INTEGER NOT NULL PRIMARY KEY, 
-                        POINTS INTEGER DEFAULT 0 NOT NULL,
-                        POLE INTEGER DEFAULT 0 NOT NULL,
-                        SUBPOLE INTEGER DEFAULT 0 NOT NULL,
-                        FAIL INTEGER DEFAULT 0 NOT NULL)""",
-        )
-        for query in sql:
-            conn.execute(query)
+        conn = database_utils.get_conn(guild.id)
+        sql = """CREATE TABLE FLAGS (
+                    ID INTEGER NOT NULL PRIMARY KEY, 
+                    POINTS INTEGER DEFAULT 0 NOT NULL,
+                    POLE INTEGER DEFAULT 0 NOT NULL,
+                    SUBPOLE INTEGER DEFAULT 0 NOT NULL,
+                    FAIL INTEGER DEFAULT 0 NOT NULL);"""
+
+        conn.execute(sql)
         conn.close()
 
     async def cog_before_invoke(self, ctx):
@@ -100,8 +97,9 @@ class Flags(commands.Cog):
     async def pole(self, ctx):
         flagstatus = self.is_flag_ready("pole", ctx.guild.id, ctx.author.id)
         if flagstatus == 1:
-            conn = self._get_conn(ctx.guild.id)
-            conn.execute("INSERT INTO FLAGS(ID,POINTS,POLE) VALUES(?,4,1) ON CONFLICT(ID) DO UPDATE SET POINTS = POINTS + 4, POLE = POLE + 1", (ctx.author.id,))
+            conn = database_utils.get_conn(ctx.guild.id)
+            conn.execute(
+                "INSERT INTO FLAGS(ID,POINTS,POLE) VALUES(?,4,1) ON CONFLICT(ID) DO UPDATE SET POINTS = POINTS + 4, POLE = POLE + 1", (ctx.author.id,))
             self.readyflags[ctx.guild.id].pole = False
             self.readyflags[ctx.guild.id].pole_maker = ctx.author.id
             await ctx.send(f'{ctx.author.mention} ha hecho la pole!')
@@ -109,13 +107,14 @@ class Flags(commands.Cog):
             await ctx.send(f'<@{self.readyflags[ctx.guild.id].pole_maker}> ya ha hecho la pole')
         elif isinstance(flagstatus, str):
             await ctx.send(f'{ctx.author.mention} ya has hecho .{flagstatus}')
-    
+
     @commands.command()
     async def subpole(self, ctx):
         flagstatus = self.is_flag_ready("subpole", ctx.guild.id, ctx.author.id)
         if flagstatus == 1:
-            conn = self._get_conn(ctx.guild.id)
-            conn.execute("INSERT INTO FLAGS(ID,POINTS,SUBPOLE) VALUES(?,2,1) ON CONFLICT(ID) DO UPDATE SET POINTS = POINTS + 2, SUBPOLE = SUBPOLE + 1", (ctx.author.id,))
+            conn = database_utils.get_conn(ctx.guild.id)
+            conn.execute(
+                "INSERT INTO FLAGS(ID,POINTS,SUBPOLE) VALUES(?,2,1) ON CONFLICT(ID) DO UPDATE SET POINTS = POINTS + 2, SUBPOLE = SUBPOLE + 1", (ctx.author.id,))
             self.readyflags[ctx.guild.id].subpole = False
             self.readyflags[ctx.guild.id].subpole_maker = ctx.author.id
             await ctx.send(f'{ctx.author.mention} ha hecho la subpole!')
@@ -128,8 +127,9 @@ class Flags(commands.Cog):
     async def fail(self, ctx):
         flagstatus = self.is_flag_ready("fail", ctx.guild.id, ctx.author.id)
         if flagstatus == 1:
-            conn = self._get_conn(ctx.guild.id)
-            conn.execute("INSERT INTO FLAGS(ID,POINTS,FAIL) VALUES(?,1,1) ON CONFLICT(ID) DO UPDATE SET POINTS = POINTS + 1, FAIL = FAIL + 1", (ctx.author.id,))
+            conn = database_utils.get_conn(ctx.guild.id)
+            conn.execute(
+                "INSERT INTO FLAGS(ID,POINTS,FAIL) VALUES(?,1,1) ON CONFLICT(ID) DO UPDATE SET POINTS = POINTS + 1, FAIL = FAIL + 1", (ctx.author.id,))
             self.readyflags[ctx.guild.id].fail = False
             self.readyflags[ctx.guild.id].fail_maker = ctx.author.id
             await ctx.send(f'{ctx.author.mention} ha hecho el fail!')
@@ -140,7 +140,7 @@ class Flags(commands.Cog):
 
     @commands.command()
     async def ranking(self, ctx):
-        conn = self._get_conn(ctx.guild.id)
+        conn = database_utils.get_conn(ctx.guild.id)
         points = conn.execute("SELECT * FROM FLAGS").fetchall()
         description = ""
 
@@ -153,22 +153,27 @@ class Flags(commands.Cog):
 
         description += """:checkered_flag: :earth_africa: **RANKING GLOBAL** :earth_africa: :checkered_flag:\n----------------------------------------\n"""
         for idx, member in enumerate(total_points):
-            if idx > 5: break
+            if idx >= 5:
+                break
             description += f"{emoticon_list[idx]}<@{member[0]}> => {member[1]}\n"
         description += """:checkered_flag: :one: **POLE** :one: :checkered_flag:\n----------------------------------------\n"""
         for idx, member in enumerate(pole_points):
-            if idx > 5: break
+            if idx >= 5:
+                break
             description += f"{emoticon_list[idx]}<@{member[0]}> => {member[2]}\n"
         description += """:checkered_flag: :two: **SUBPOLE** :two: :checkered_flag:\n----------------------------------------\n"""
         for idx, member in enumerate(subpole_points):
-            if idx > 5: break
+            if idx >= 5:
+                break
             description += f"{emoticon_list[idx]}<@{member[0]}> => {member[3]}\n"
         description += """:checkered_flag: :three: **FAIL** :three: :checkered_flag:\n----------------------------------------\n"""
         for idx, member in enumerate(fail_points):
-            if idx > 5: break
+            if idx >= 5:
+                break
             description += f"{emoticon_list[idx]}<@{member[0]}> => {member[4]}\n"
 
-        embed = discord.Embed(colour=discord.Colour.blue(), description=description)
+        embed = discord.Embed(colour=discord.Colour.blue(),
+                              description=description)
         await ctx.send(embed=embed)
 
 
--- main.py
+++ main.py
@@ -1,83 +1,58 @@
-from os import getenv #Needed to get the arguments
-from discord.ext import commands, tasks #The library for a discord bot (along with tasks to schedule loops)
-#Imports functions from the other files on this project
+from os import getenv  # Needed to get the arguments
+# The library for a discord bot (along with tasks to schedule loops)
+from discord.ext import commands
+# Imports functions from the other files on this project
 
-import russian_roulette
-from datetime import datetime, timedelta #Needed for scheduling
+import misc
+from datetime import datetime, timedelta  # Needed for scheduling
 import music
 import flags
 
-bot = commands.Bot(command_prefix='.', help_command=None) #Creates the bot object
+# Creates the bot object
+bot = commands.Bot(command_prefix='.', help_command=None)
 token = str(getenv('DISCORD_API_KEY'))
 
 
 @bot.event
-async def on_ready(): #Tells you when its ready
+async def on_ready():  # Tells you when its ready
     print('We have logged in as {0.user}'.format(bot))
     flags.setup(bot)
     music.setup(bot)
-    russian_roulette.setup(bot)
+    misc.setup(bot)
 
-@bot.event
-async def on_member_join(member): #Restores (or tries to) restore the roles of the person who joins
-    try:
-        await restoreRoles(member)
-    except:
-        pass
 
 @bot.command(name='ping')
-async def ping_command(ctx:commands.Context):
+async def ping_command(ctx: commands.Context):
     await ctx.channel.send('pong')
+
 
 @bot.command(name='version')
 async def version_command(ctx):
-    await ctx.channel.send('dummybot(py) v1.92 NO STABLE') #Sends current bot version over discord
+    # Sends current bot version over discord
+    await ctx.channel.send('dummybot(py) v1.93 NO STABLE')
+
 
 @bot.command(name='stop')
 async def stop_command(ctx):
-    if(ctx.message.author == ctx.guild.owner): #Only if the owner asks to, the bot stops
+    if(ctx.message.author == ctx.guild.owner):
         await bot.close()
 
-
-@bot.command(name='dato', aliases=['fact'])
-async def dato_command(ctx):
-    await ctx.channel.send(getFact()) #Gets a fact from the database and sends it
-
-
-@bot.command(name='datoadd', aliases=['adddato', 'addfact', 'añadirdato'])
-async def datoadd_command(ctx, *, arg1):
-    try: #Tries to add the fact to the database
-        addFact(arg1)
-        await ctx.channel.send('Dato añadido!')
-    except:
-        await ctx.channel.send('Ha ocurrido un error al añadir tu dato') #If something goes wrong it will throw an error
-
-
-
-
-@bot.command(name='suicide')
-async def suicide_command(ctx):
-    saveRoles(ctx) #Saves its roles before kicking
-    await ctx.channel.send('<@{0}> decidió que seguir viviendo no valía la pena'.format(ctx.message.author.id))
-    await ctx.message.author.send(await ctx.channel.create_invite(max_uses=1)) #Sends him/her an invite back to the server
-    try:
-        await ctx.message.author.kick() #Kicks
-    except:
-        await ctx.channel.send('No tengo permisos suficientes!') #If it has no perms it will show this
-
-@bot.command(name='poletime', aliases=['timepole','nextpole','timeleft']) #This whole function just calculates the time between now and 00:00
-async def poletime_command(ctx:commands.Context):
+# Calculates the time between now and 00:00
+@bot.command(name='poletime', aliases=['timepole', 'nextpole', 'timeleft'])
+async def poletime_command(ctx: commands.Context):
     now = datetime.now()
     future = datetime(now.year, now.month, now.day, 00, 00)
     if now.hour >= 00 and now.minute > 00:
         future += timedelta(days=1)
     await ctx.channel.send('Queda {0} hasta la siguiente pole'.format(future - now))
 
+
 @bot.command(name='satisfactory')
-async def satisfactory_command(ctx:commands.Context):
+async def satisfactory_command(ctx: commands.Context):
     now = datetime.now()
     future = datetime(2020, 6, 9, 00, 00)
     await ctx.channel.send('Quedan {0} hasta que salga satisfactory (approx)'.format(future - now))
+
 
 @bot.command(name='google')
 async def google_command(ctx, *, arg1):
