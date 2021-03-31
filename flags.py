6dcf595c92582d7bd99336528da1a97fd3dbd08e
Josep Mengual <josepmengual@protonmail.com>
2021-03-31 23:35:23 +0200

    Rewrite pole functionality

--- flags.py
+++ flags.py
@@ -10,6 +10,7 @@ from operator import itemgetter
 
 class Flags(commands.Cog):
     readyflags = {}
+    flag_number_to_string = ["pole", "subpole", "fail"]
 
     def __init__(self, bot):
         self.bot = bot
@@ -23,36 +24,24 @@ class Flags(commands.Cog):
         self.reset_flags.start()
 
     class FlagChecks():
-        pole = True
-        subpole = True
-        fail = True
+        flags = [True, True, True]
+        makers = [0, 0, 0]
 
-        pole_maker = 0
-        subpole_maker = 0
-        fail_maker = 0
-
-        def cases(self, flagtype):
-            if flagtype == "pole":
-                return (("subpole", self.subpole, self.subpole_maker), ("fail", self.fail, self.fail_maker))
-            if flagtype == "subpole":
-                return (("pole", self.pole, self.pole_maker), ("fail", self.fail, self.fail_maker))
-            if flagtype == "fail":
-                return (("pole", self.pole, self.pole_maker), ("subpole", self.subpole, self.subpole_maker))
-
-    # 0 = Flag is unavalaible, 1 = Flag is avalaible, *FlagType* = A user has already taken *FlagType*
-    def is_flag_ready(self, flagtype, guild, maker):
+    # Returns a tuple [Whether the flag is taken, [If you have already taken a flag, The flag you've taken]]
+    def get_flag_info(self, flagtype, guild, maker):
         if guild in self.readyflags:
-            if not getattr(self.readyflags[guild], flagtype):
-                return 0
-            for pack in self.readyflags[guild].cases(flagtype):
-                if pack[2] == maker:
-                    return pack[0]
-            return 1
+            guild_flags = self.readyflags[guild]
+            if guild_flags.flags[flagtype] != True:
+                return (False, guild_flags.makers[flagtype])
+            elif maker in (guild_flags.makers[:flagtype] + guild_flags.makers[flagtype:]):
+                return (False, (True, guild_flags.index(maker)))
+            else:
+                return (True, None)
 
         else:
             self.readyflags[guild] = self.FlagChecks()
-            setattr(self.readyflags[guild], f"{flagtype}", False)
-            return 0
+            self.readyflags[guild].flags[flagtype] = False
+            return (True, None)
 
     @tasks.loop(hours=24)
     async def reset_flags(self):
@@ -95,48 +84,51 @@ class Flags(commands.Cog):
 
     @commands.command()
     async def pole(self, ctx):
-        flagstatus = self.is_flag_ready("pole", ctx.guild.id, ctx.author.id)
-        if flagstatus == 1:
+        flag_info = self.get_flag_info(0, ctx.guild.id, ctx.author.id)
+        guild_flags = self.readyflags[ctx.guild.id]
+        if flag_info[0] == True:
             conn = database_utils.get_conn(ctx.guild.id)
             conn.execute(
                 "INSERT INTO FLAGS(ID,POINTS,POLE) VALUES(?,4,1) ON CONFLICT(ID) DO UPDATE SET POINTS = POINTS + 4, POLE = POLE + 1", (ctx.author.id,))
-            self.readyflags[ctx.guild.id].pole = False
-            self.readyflags[ctx.guild.id].pole_maker = ctx.author.id
+            guild_flags.flags[0] = False
+            guild_flags.makers[0] = ctx.author.id
             await ctx.send(f'{ctx.author.mention} ha hecho la pole!')
-        elif flagstatus == 0:
-            await ctx.send(f'<@{self.readyflags[ctx.guild.id].pole_maker}> ya ha hecho la pole')
-        elif isinstance(flagstatus, str):
-            await ctx.send(f'{ctx.author.mention} ya has hecho .{flagstatus}')
+        elif flag_info[1][0] == True:
+            await ctx.send(f'{ctx.author.mention} ya has hecho .{self.flag_number_to_string[flag_info[1][1]]}')
+        elif flag_info[0] == False:
+            await ctx.send(f'<@{guild_flags.makers[0]}> ya ha hecho la pole')
 
     @commands.command()
     async def subpole(self, ctx):
-        flagstatus = self.is_flag_ready("subpole", ctx.guild.id, ctx.author.id)
-        if flagstatus == 1:
+        flag_info = self.get_flag_info(1, ctx.guild.id, ctx.author.id)
+        guild_flags = self.readyflags[ctx.guild.id]
+        if flag_info[0] == True:
             conn = database_utils.get_conn(ctx.guild.id)
             conn.execute(
                 "INSERT INTO FLAGS(ID,POINTS,SUBPOLE) VALUES(?,2,1) ON CONFLICT(ID) DO UPDATE SET POINTS = POINTS + 2, SUBPOLE = SUBPOLE + 1", (ctx.author.id,))
-            self.readyflags[ctx.guild.id].subpole = False
-            self.readyflags[ctx.guild.id].subpole_maker = ctx.author.id
-            await ctx.send(f'{ctx.author.mention} ha hecho la subpole!')
-        elif flagstatus == 0:
-            await ctx.send(f'<@{self.readyflags[ctx.guild.id].subpole_maker}> ya ha hecho la subpole')
-        elif isinstance(flagstatus, str):
-            await ctx.send(f'{ctx.author.mention} ya has hecho .{flagstatus}')
+            guild_flags.flags[1] = False
+            guild_flags.makers[1] = ctx.author.id
+            await ctx.send(f'{ctx.author.mention} ha hecho la pole!')
+        elif flag_info[1][0] == True:
+            await ctx.send(f'{ctx.author.mention} ya has hecho .{self.flag_number_to_string[flag_info[1][1]]}')
+        elif flag_info[0] == False:
+            await ctx.send(f'<@{guild_flags.makers[1]}> ya ha hecho la pole')
 
     @commands.command()
     async def fail(self, ctx):
-        flagstatus = self.is_flag_ready("fail", ctx.guild.id, ctx.author.id)
-        if flagstatus == 1:
+        flag_info = self.get_flag_info(2, ctx.guild.id, ctx.author.id)
+        guild_flags = self.readyflags[ctx.guild.id]
+        if flag_info[0] == True:
             conn = database_utils.get_conn(ctx.guild.id)
             conn.execute(
                 "INSERT INTO FLAGS(ID,POINTS,FAIL) VALUES(?,1,1) ON CONFLICT(ID) DO UPDATE SET POINTS = POINTS + 1, FAIL = FAIL + 1", (ctx.author.id,))
-            self.readyflags[ctx.guild.id].fail = False
-            self.readyflags[ctx.guild.id].fail_maker = ctx.author.id
-            await ctx.send(f'{ctx.author.mention} ha hecho el fail!')
-        elif flagstatus == 0:
-            await ctx.send(f'<@{self.readyflags[ctx.guild.id].fail_maker}> ya ha hecho el fail')
-        elif isinstance(flagstatus, str):
-            await ctx.send(f'{ctx.author.mention} ya has hecho .{flagstatus}')
+            guild_flags.flags[2] = False
+            guild_flags.makers[2] = ctx.author.id
+            await ctx.send(f'{ctx.author.mention} ha hecho la pole!')
+        elif flag_info[1][0] == True:
+            await ctx.send(f'{ctx.author.mention} ya has hecho .{self.flag_number_to_string[flag_info[1][1]]}')
+        elif flag_info[0] == False:
+            await ctx.send(f'<@{guild_flags.makers[2]}> ya ha hecho la pole')
 
     @commands.command()
     async def ranking(self, ctx):
